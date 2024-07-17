20e2b46626b196be8c88885c13c9e8a6daedf4e2
Egor <egor@philippov.ca>
2024-07-17 14:01:05 -0700

    update github export to support next.js v13+

--- packages/github/src/builder.ts
+++ packages/github/src/builder.ts
@@ -1,8 +1,6 @@
 import execa from 'execa';
 import { writeFileSync } from 'fs';
 
-const NEXT_EXPORT_CMD = ['export'];
-
 const build = async (
   { cmd, cwd, args }: BuildOptions,
   debug?: (message: string) => void
@@ -18,22 +16,11 @@ const build = async (
   }
 
   // run the build
-  let subprocess = execa(cmd, args, {
+  const subprocess = execa(cmd, args, {
     cwd,
     env: {
       NODE_OPTIONS: '--max_old_space_size=3000',
     },
-  });
-
-  if (debug && subprocess.stdout) {
-    subprocess.stdout.pipe(process.stdout);
-  }
-
-  await subprocess;
-
-  // export the static assets from the build
-  subprocess = execa(cmd, NEXT_EXPORT_CMD, {
-    cwd,
   });
 
   if (debug && subprocess.stdout) {
